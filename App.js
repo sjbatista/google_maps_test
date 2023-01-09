c38b9b842842f2fa6630844a862b7b565b726e4a
sjbatista <salomaojunio@hotmail.com>
2023-01-09 18:57:36 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test</Text>
       <StatusBar style="auto" />
     </View>
   );
